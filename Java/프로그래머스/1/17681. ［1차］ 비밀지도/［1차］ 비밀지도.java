e25fa19f3f74efe488e2de86d98f789f6b253d7c
Soo Lee <dltn990930@naver.com>
2024-11-20 09:52:53 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 77.4 MB -BaekjoonHub

--- Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,34 +1,31 @@
-public class Solution {
-	public static String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        boolean[][] secMap1 = new boolean[n][n]; // 1번 지도 // true면 벽, false면 공백으로 봄.        
-        boolean[][] secMap2 = new boolean[n][n]; // 2번 지도
-        
-        for(int i=0; i<n; i++) {
-        	int num1 = arr1[i];
-        	int num2 = arr2[i];
-        	for(int idx=1; idx<=n; idx++) {        		
-        		secMap1[i][n-idx] = (num1 % 2==1) ? true: false;        		
-        		num1 /=2;
-        		
-        		secMap2[i][n-idx] = (num2 % 2==1) ? true: false;        		
-        		num2 /=2;
-        	}        	
-        }               
-        
-        for(int i=0; i<n; i++) {
-        	StringBuilder sb = new StringBuilder();
-        	for(int j=0; j<n; j++) {
-        		if(secMap1[i][j]==true || secMap2[i][j]==true) {
-        			sb.append('#');
-        		}
-        		else {
-        			sb.append(' ');
-        		}
-        	}
-        	answer[i] = sb.toString();
-        }        
-        
-        return answer;
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] resultMap = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            // OR 연산으로 두 지도를 합침
+            int combinedMap = arr1[i] | arr2[i];
+
+            // OR 연산 결과를 지도 문자열("#", " ")로 변환
+            resultMap[i] = toMapRow(combinedMap, n);
+        }
+
+        return resultMap;
+    }
+
+    // OR 연산 결과를 지도 문자열("#", " ")로 변환
+    private String toMapRow(int combinedMap, int length) {
+        StringBuilder row = new StringBuilder();
+
+        for (int bit = length - 1; bit >= 0; bit--) {
+            // 현재 비트가 1이면 "#", 아니면 " "
+            if ((combinedMap & (1 << bit)) != 0) {
+                row.append('#');
+            } else {
+                row.append(' ');
+            }
+        }
+
+        return row.toString();
     }
 }
