6ed2a41cf8bd315cadb57bad6f234e14ce60dd76
Soo Lee <dltn990930@naver.com>
2024-11-29 09:26:49 +0900

    [level 2] Title: 광물 캐기, Time: 38.63 ms, Memory: 93.1 MB -BaekjoonHub

--- Java/프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ Java/프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,82 +1,270 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] picks, String[] minerals) {
-        MiningOptimizer optimizer = new MiningOptimizer(picks, minerals);
-        return optimizer.calculateMinFatigue();
-    }
-}
-
-class MiningOptimizer {
-    private final int[] picks;
-    private final String[] minerals;
-    private final int[][] fatigue;
-    private List<MineralGroup> groups;
-
-    public MiningOptimizer(int[] picks, String[] minerals) {
-        this.picks = picks;
-        this.minerals = minerals;
-        this.fatigue = new int[][]{
-                {1, 1, 1},
-                {5, 1, 1},
-                {25, 5, 1}
-        };
-        this.groups = new ArrayList<>();
-        groupMinerals();
+        return new Mine().calculateMinFatigue(picks, minerals);
     }
 
-    private void groupMinerals() {
-        int totalPicks = Arrays.stream(picks).sum();
-        for (int i = 0; i < minerals.length && totalPicks > 0; i += 5) {
-            int diamond = 0, iron = 0, stone = 0;
-            for (int j = i; j < i + 5 && j < minerals.length; j++) {
-                switch (minerals[j]) {
-                    case "diamond": diamond++; break;
-                    case "iron": iron++; break;
-                    case "stone": stone++; break;
+    private static class Mine {
+        public int calculateMinFatigue(int[] picks, String[] minerals) {
+            return calculateMinFatigue(
+                    Picks.of(picks),
+                    Minerals.of(minerals)
+            );
+        }
+
+        public int calculateMinFatigue(Picks picks, Minerals minerals) {
+            return minerals.digInMinFatigue(picks);
+        }
+    }
+
+    private static class Minerals {
+        private final List<Mineral> minerals;
+
+        public Minerals(List<Mineral> minerals) {
+            this.minerals = minerals;
+        }
+
+        public static Minerals of(String[] minerals) {
+            return Arrays.stream(minerals)
+                    .map(MineralType::findType)
+                    .map(Mineral::new)
+                    .collect(Collectors.collectingAndThen(Collectors.toList(), Minerals::new));
+        }
+
+        public int digFrom(Pick pick) {
+            return minerals.stream()
+                    .mapToInt(pick::dig)
+                    .sum();
+        }
+
+        public int digInMinFatigue(Picks picks) {
+            int fatigue = 0;
+
+            List<Minerals> toDig = IntStream.range(0, Integer.min(picks.size(),
+                            minerals.size() / 5 + (minerals.size() % 5 > 0 ? 1 : 0)))
+                    .mapToObj(i -> minerals.subList(i * 5, Integer.min(5 + i * 5, minerals.size())))
+                    .map(Minerals::new)
+                    .sorted(compareTo().reversed())
+                    .collect(Collectors.toList());
+
+            for (Minerals next : toDig) {
+                Optional<Pick> pick = picks.pollFor(next);
+
+                if (pick.isEmpty()) {
+                    break;
                 }
+                fatigue += pick.get().dig(next);
             }
-            groups.add(new MineralGroup(diamond, iron, stone));
-            totalPicks--;
+
+            return fatigue;
         }
 
-        // 돌 곡괭이 기준으로 내림차순 정렬
-        groups.sort((g1, g2) -> g2.calculateFatigue(fatigue[2]) - g1.calculateFatigue(fatigue[2]));
+        private Comparator<Minerals> compareTo() {
+            return Comparator.comparingInt(a -> a.minerals.stream()
+                    .mapToInt(Mineral::weight)
+                    .sum());
+        }
     }
 
-    public int calculateMinFatigue() {
-        int totalFatigue = 0;
+    private static class Mineral {
+        private static final Map<MineralType, Function<Pick, Integer>> DIGGING = Map.of(
+                MineralType.DIAMOND, Pick::digDiamond,
+                MineralType.IRON, Pick::digIron,
+                MineralType.STONE, Pick::digStone
+        );
+        private final MineralType mineralType;
 
-        for (MineralGroup group : groups) {
-            int pickIndex = getAvailablePickIndex();
-            if (pickIndex == -1) break;
-            totalFatigue += group.calculateFatigue(fatigue[pickIndex]);
-            picks[pickIndex]--;
+        public Mineral(MineralType mineralType) {
+            this.mineralType = mineralType;
         }
 
-        return totalFatigue;
-    }
-
-    private int getAvailablePickIndex() {
-        for (int i = 0; i < picks.length; i++) {
-            if (picks[i] > 0) return i;
+        public int digFrom(Pick pick) {
+            return DIGGING.get(mineralType).apply(pick);
         }
-        return -1;
-    }
-}
 
-class MineralGroup {
-    private final int diamond;
-    private final int iron;
-    private final int stone;
-
-    public MineralGroup(int diamond, int iron, int stone) {
-        this.diamond = diamond;
-        this.iron = iron;
-        this.stone = stone;
+        public int weight() {
+            return mineralType.getWeight();
+        }
     }
 
-    public int calculateFatigue(int[] pickFatigue) {
-        return diamond * pickFatigue[0] + iron * pickFatigue[1] + stone * pickFatigue[2];
+    private enum MineralType {
+        DIAMOND("diamond", 25),
+        IRON("iron", 5),
+        STONE("stone", 1);
+
+        private final String type;
+        private final int weight;
+
+        MineralType(String type, int weight) {
+            this.type = type;
+            this.weight = weight;
+        }
+
+        public static MineralType findType(String type) {
+            return Arrays.stream(values())
+                    .filter(mineralType -> mineralType.type.equals(type))
+                    .findAny()
+                    .orElseThrow();
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+    }
+
+    private static class Picks {
+        private final List<Pick> picks;
+
+        public Picks(List<Pick> picks) {
+            this.picks = picks;
+        }
+
+        public static Picks of(int[] picks) {
+            return new Picks(List.of(
+                    new DiamondPick(picks[0]),
+                    new IronPick(picks[1]),
+                    new StonePick(picks[2])
+            ));
+        }
+
+        public Optional<Pick> pollFor(Minerals minerals) {
+            return picks.stream()
+                    .filter(Pick::isNotEmpty)
+                    .min(Comparator.comparingInt(pick -> pick.dig(minerals)))
+                    .map(pick -> {
+                        pick.decrease();
+                        return pick;
+                    });
+        }
+
+        public int size() {
+            return picks.stream()
+                    .mapToInt(Pick::getSize)
+                    .sum();
+        }
+    }
+
+    private interface Pick {
+        int dig(Minerals minerals);
+
+        int dig(Mineral mineral);
+
+        int digDiamond();
+
+        int digIron();
+
+        int digStone();
+
+        void decrease();
+
+        boolean isNotEmpty();
+
+        int getSize();
+    }
+
+    private static abstract class AbstractPick implements Pick {
+        protected MineralType mineralType;
+        protected Integer count;
+
+        public AbstractPick(MineralType mineralType, int count) {
+            this.mineralType = mineralType;
+            this.count = count;
+        }
+
+        @Override
+        public int dig(Minerals minerals) {
+            return minerals.digFrom(this);
+        }
+
+        @Override
+        public int dig(Mineral mineral) {
+            return mineral.digFrom(this);
+        }
+
+        @Override
+        public void decrease() {
+            count--;
+        }
+
+        @Override
+        public boolean isNotEmpty() {
+            return count > 0;
+        }
+
+        @Override
+        public int getSize() {
+            return count;
+        }
+    }
+
+    private static class DiamondPick extends AbstractPick {
+        public DiamondPick(int count) {
+            super(MineralType.DIAMOND, count);
+        }
+
+        @Override
+        public int digDiamond() {
+            return 1;
+        }
+
+        @Override
+        public int digIron() {
+            return 1;
+        }
+
+        @Override
+        public int digStone() {
+            return 1;
+        }
+    }
+
+    private static class IronPick extends AbstractPick {
+        public IronPick(int count) {
+            super(MineralType.IRON, count);
+        }
+
+        @Override
+        public int digDiamond() {
+            return 5;
+        }
+
+        @Override
+        public int digIron() {
+            return 1;
+        }
+
+        @Override
+        public int digStone() {
+            return 1;
+        }
+    }
+
+    private static class StonePick extends AbstractPick {
+        public StonePick(int count) {
+            super(MineralType.STONE, count);
+        }
+
+        @Override
+        public int digDiamond() {
+            return 25;
+        }
+
+        @Override
+        public int digIron() {
+            return 5;
+        }
+
+        @Override
+        public int digStone() {
+            return 1;
+        }
     }
 }
