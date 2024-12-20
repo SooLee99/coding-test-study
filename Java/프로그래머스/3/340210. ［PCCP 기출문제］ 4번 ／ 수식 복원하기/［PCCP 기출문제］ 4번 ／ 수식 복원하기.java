c2c9fe66f58e17c0dd52b6220a176d8a1809d83a
Soo Lee <dltn990930@naver.com>
2024-12-20 16:17:35 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 2.40 ms, Memory: 78.8 MB -BaekjoonHub

--- Java/프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ Java/프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -1,130 +1,123 @@
 import java.util.*;
 
 class Solution {
+    // 2~9진법중 하나
+    // 덧셈, 뺄셈 수식만 나옴
+    // 불확실한 결과값은 ?로 표시
     public String[] solution(String[] expressions) {
-        List<String> completeExpression = new ArrayList<>();
-        List<String> problemExpression = new ArrayList<>();
+        // 결과값 지워진 수식 저장필요
+        ArrayDeque<String> uncompleteExpression = new ArrayDeque<>();
 
+        // 초기
+        boolean[] valid = new boolean[10];
+        Arrays.fill(valid, true);
+        valid[0] = false;
+        valid[1] = false;
+
+        // 각 자리수 최대 숫자 + 1 진법 ~ 9 진법
         for (String expression : expressions) {
-            if (!expression.contains("X")) {
-                completeExpression.add(expression);
-            } else {
-                problemExpression.add(expression);
+            String[] e = expression.split(" ");
+            // System.out.println(Arrays.toString(e));
+
+            // A +- B = C 이므로 0, 1, 2, 3, 4
+            boolean complete = true;
+            int maxValue = 0;
+            for (int i = 0; i < 5; i += 2) {
+                if (!"X".equals(e[i])) {
+                    maxValue = Math.max(maxValue, getMaxValue(e[i]));
+                } else {
+                    // X 있으면 큐에 저장
+                    uncompleteExpression.offer(expression);
+                    complete = false;
+                }
+            }
+
+            // maxValue를 통해 진법 갱신
+            for (int i = 2; i <= maxValue; i++) {
+                valid[i] = false;
+            }
+
+            // 현재 진법 범위로 실제 계산 해봄
+            if (!complete) {
+                continue;
+            }
+
+            for (int jinbup = 2; jinbup <= 9; jinbup++) {
+                if (!valid[jinbup]) {
+                    continue;
+                }
+
+                int result = Integer.parseInt(e[0], jinbup);
+                if ("+".equals(e[1])) {
+                    result += Integer.parseInt(e[2], jinbup);
+                } else {
+                    result -= Integer.parseInt(e[2], jinbup);
+                }
+                int trueResult = Integer.parseInt(e[4], jinbup);
+
+                // System.out.printf("진법 %d : %d = %d\n", jinbup, result, trueResult);
+
+                if (trueResult != result) {
+                    valid[jinbup] = false;
+                }
             }
         }
 
-        String[] result = new String[problemExpression.size()];
-        Map<Integer, Integer> expressionMap = new HashMap<>();
-        expressionMap.put(2, 0); expressionMap.put(3, 0); expressionMap.put(4, 0);
-        expressionMap.put(5, 0); expressionMap.put(6, 0); expressionMap.put(7, 0);
-        expressionMap.put(8, 0); expressionMap.put(9, 0);
+        // System.out.println(Arrays.toString(valid));
 
-        int maxDigitInComplete = 0;
-        int minBaseInComplete = 0;
-
-        for (String ex : completeExpression) {
-            String[] parts = ex.split(" ");
-            int maxDigit= Math.max(getMaxDigit(parts[0]), getMaxDigit(parts[2]));
-            maxDigit = Math.max(maxDigit, getMaxDigit(parts[4]));
-            maxDigitInComplete = Math.max(maxDigit, maxDigitInComplete);
-        }
-        minBaseInComplete = maxDigitInComplete + 1;
-
-
-        for (String ex : completeExpression) {
-            String[] parts = ex.split(" ");
-            String op = parts[1];
-
-            for (int base = minBaseInComplete; base <= 9; base++) {
-                check(parts, base, op, expressionMap);
-            }
-        }
-
-
-        int maxDigitInProblem = 0;
-        int minBaseInProblem = 0;
-
-        for (String ex : problemExpression) {
-            String[] parts = ex.split(" ");
-            int maxDigit= Math.max(getMaxDigit(parts[0]), getMaxDigit(parts[2]));
-            maxDigitInProblem = Math.max(maxDigit, maxDigitInProblem);
-        }
-        minBaseInProblem = maxDigitInProblem + 1;
-
-
-        for (int i = 0; i < problemExpression.size(); i++) {
-            String[] parts = problemExpression.get(i).split(" ");
-            String op = parts[1];
-
+        String[] answer = new String[uncompleteExpression.size()];
+        int idx = 0;
+        // 남은 수식 진법으로 계산
+        while (!uncompleteExpression.isEmpty()) {
+            String expression = uncompleteExpression.poll();
             Set<String> resultSet = new HashSet<>();
+            String[] e = expression.split(" ");
 
-            for (int base = minBaseInProblem; base <= 9; base++) {
-                int value = expressionMap.get(base);
-                if (value == completeExpression.size()) {
-                    checkSet(parts, base, op, resultSet);
+            for (int jinbup = 2; jinbup <= 9; jinbup++) {
+                if (!valid[jinbup]) {
+                    continue;
                 }
+
+                int result = Integer.parseInt(e[0], jinbup);
+                if ("+".equals(e[1])) {
+                    result += Integer.parseInt(e[2], jinbup);
+                } else {
+                    result -= Integer.parseInt(e[2], jinbup);
+                }
+
+                e[4] = Integer.toString(result, jinbup);
+
+                resultSet.add(e[4]);
             }
 
-
-            if (resultSet.size() != 1) {
-                result[i] = parts[0] + " " + parts[1] + " " + parts[2] + " " + parts[3] + " " + "?";
+            // resultSet이 2이상이면 ?로 결과
+            if (resultSet.size() >= 2) {
+                answer[idx] = expression.replace("X", "?");
             } else {
-                Iterator<String> it = resultSet.iterator();
-                if (it.hasNext()) {
-                    String value = it.next();
-                    result[i] = parts[0] + " " + parts[1] + " " + parts[2] + " " + parts[3] + " " + value;
-                }
+                answer[idx] = expression.replace("X", e[4]);
             }
+            idx++;
         }
-        return result;
+
+        return answer;
     }
 
-    private void checkSet(String[] parts, int base, String op, Set<String> resultSet) {
-         if (!isValidNumber(parts[0], base) || !isValidNumber(parts[2], base)) {
-            return;
+    private String convert(int num, int jinbup) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.append(num % jinbup);
+            num /= jinbup;
         }
-        int num1 = Integer.parseInt(parts[0], base);
-        int num2 = Integer.parseInt(parts[2], base);
-
-
-        int res = op.equals("+") ? num1 + num2 : num1 - num2;
-
-        String ans = Integer.toString(res, base);
-        resultSet.add(ans);
+        return sb.reverse().toString();
     }
 
-    private int getMaxDigit(String number) {
-        int maxDigit = 0;
-        for (char c : number.toCharArray()) {
-            if (Character.isDigit(c)) {
-                maxDigit = Math.max(maxDigit, Character.getNumericValue(c));
-            }
+    private int getMaxValue(String num) {
+        int n = Integer.parseInt(num);
+        int maxValue = 0;
+        while (n > 0) {
+            maxValue = Math.max(maxValue, n % 10);
+            n = n / 10;
         }
-        return maxDigit;
-    }
-
-    private boolean isValidNumber(String number, int base) {
-        for (char c : number.toCharArray()) {
-            int digit = Character.getNumericValue(c);
-            if (digit < 0 || digit >= base) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void check(String[] parts, int base, String op, Map<Integer, Integer> expressionMap) {
-        if (!isValidNumber(parts[0], base) || !isValidNumber(parts[2], base) || !isValidNumber(parts[4], base)){
-            return;
-        }
-        int num1 = Integer.parseInt(parts[0], base);
-        int num2 = Integer.parseInt(parts[2], base);
-        int res = op.equals("+") ? num1 + num2 : num1 - num2;
-
-        String ans = Integer.toString(res, base);
-
-        if (ans.equals(parts[4])) {
-            expressionMap.merge(base, 1, Integer::sum);
-        }
+        return maxValue;
     }
 }
