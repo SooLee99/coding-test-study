cc4c4eba2a08fc04894be7f34092e263b828330d
Soo Lee <dltn990930@naver.com>
2025-02-13 16:19:44 +0900

    [level 3] Title: 숫자 게임, Time: 113.91 ms, Memory: 65.9 MB -BaekjoonHub

--- Java/프로그래머스/3/12987. 숫자 게임/README.md
+++ Java/프로그래머스/3/12987. 숫자 게임/README.md
@@ -1,6 +1,6 @@
 # [level 3] 숫자 게임 - 12987 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 12일 15:29:37
+2025년 02월 13일 16:19:41
 
 ### 문제 설명
 
--- Java/프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ Java/프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -5,14 +5,13 @@ public class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
         int len = A.length - 1;
-        int index = len;
 
         Arrays.sort(A);
         Arrays.sort(B);
-        for(int i=len; i>=0; i--) {
-            if(B[index] > A[i]) {
+
+        for (int i = len; i >= 0; i--) {
+            if (B[index] > A[i]) {
                 answer++;
-                index--;
             }
         }
         return answer;
