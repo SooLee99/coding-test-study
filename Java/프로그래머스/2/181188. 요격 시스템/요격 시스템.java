2e17113bbc2a433621711ea0e4ab1760345233b8
Soo Lee <dltn990930@naver.com>
2025-02-21 16:22:19 +0900

    [level 2] Title: 요격 시스템, Time: 565.86 ms, Memory: 171 MB -BaekjoonHub

--- Java/프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ Java/프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -3,10 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        
-        Arrays.sort(targets, (o1, o2) -> { return o1[1]-o2[1]; });
-        
+
+        Arrays.sort(targets, (o1, o2) -> { 
+            return o1[1] - o2[1]; 
+        });
+
         int last = -1;
+        
         for (int[] target : targets) {
             if (last == -1) {
                 answer++;
@@ -15,11 +18,11 @@ class Solution {
             }
             
             if (last >= target[0] && last <= target[1]) continue;
-            
             answer++;
             last = target[1] - 1;
         }
         
+        // 모든 폭격 미사일 구간을 커버하는 데 필요한 최소 요격 미사일 수를 반환합니다.
         return answer;
     }
 }
