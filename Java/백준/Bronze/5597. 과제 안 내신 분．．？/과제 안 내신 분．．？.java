c14d3bd297ee764424e360ed154c8e7fd719bc8a
Soo Lee <soo_tack_overflow99@naver.com>
2025-06-22 18:53:45 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- Java/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ Java/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Boolean[] arr = new Boolean[31];
+		boolean[] arr = new boolean[31];
 
 		// 총 30명 중에 과제 제출한 28명 확인 후 안낸 2명 확인
 		for(int i = 0; i < 28; i++) {
@@ -13,11 +13,11 @@ public class Main {
 			int attendanceNo = Integer.parseInt(br.readLine());
 			arr[attendanceNo] = true;
 		}
-		
+
 		// 1부터 30까지 반복하면서 제출하지 않은 학생의 번호를 출력
 		for(int i = 1; i <= 30; i++) {
-			// 배열의 해당 인덱스가 null이면 제출하지 않은 학생
-			if(arr[i] == null)
+			// 배열의 해당 인덱스가 false이면 제출하지 않은 학생
+			if(Boolean.FALSE.equals(arr[i]))
 				System.out.println(i);
 		}
 	}
